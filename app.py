5f4c90f02765314a000a66733eb603557b2feea2
Oliver Yao <a83117700@gmail.com>
2018-05-28 23:16:50 +0800

    luis

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def handle_text_message(event):                  # default
                 )
             line_bot_api.reply_message(event.reply_token,msg)
         elif(text == '紀錄食物喜好'):
-            continue
+            pass
         elif(type(text_entity) is str):
             msg = text_entity
             message = TextSendMessage(text = msg)
