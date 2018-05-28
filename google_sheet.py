2dfafad59731588dbfdd62f4450bd02e96c3c2cf
Oliver Yao <a83117700@gmail.com>
2018-05-29 02:21:23 +0800

    luis

--- google_sheet.py
+++ google_sheet.py
@@ -3,23 +3,23 @@ class google_sheet(object):
 		super(google_sheet, self).__init__()
 		self.arg = arg
 		
-	import gspread
-	from oauth2client.service_account import ServiceAccountCredentials
-	import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import time
 
-	auth_json_path = 'sweet-man-d9c2b8272f1f.json'
-	gss_scopes = ['https://spreadsheets.google.com/feeds']
+auth_json_path = 'sweet-man-d9c2b8272f1f.json'
+gss_scopes = ['https://spreadsheets.google.com/feeds']
 
-	def auth_gss_client(path, scopes):
-	    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scopes)
-	    return gspread.authorize(credentials)
-	
-	def update_sheet(ID, food, like, flavor, size, store):
-		date = time.strftime("%c")
-		spreadsheet_key = '1B-Ghm54KLT--4qgIfhn3aUfuJlnzEGVnWski_HqJhIA'
+def auth_gss_client(path, scopes):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scopes)
+    return gspread.authorize(credentials)
 
-		gss_client = self.auth_gss_client(auth_json_path, gss_scopes)
-		wks = gss_client.open_by_key(key)
-		sheet = wks.sheet1
-		sheet.insert_row([date, ID, food, like, flavor, size, store], 2)
+def update_sheet(ID, food, like, flavor, size, store):
+	date = time.strftime("%c")
+	spreadsheet_key = '1B-Ghm54KLT--4qgIfhn3aUfuJlnzEGVnWski_HqJhIA'
+
+	gss_client = self.auth_gss_client(auth_json_path, gss_scopes)
+	wks = gss_client.open_by_key(key)
+	sheet = wks.sheet1
+	sheet.insert_row([date, ID, food, like, flavor, size, store], 2)
 	
